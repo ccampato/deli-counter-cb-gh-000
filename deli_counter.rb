c72fbfdf8905b9929cf310bf8e48db93d507c2a1
Caio Sanches <ccampato@gmail.com>
2018-04-20 08:43:17 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,13 +4,11 @@ def line(a)
   if a.count == 0
     puts "The line is currently empty."
   else
-    puts "The line is currently:"+a.index+a
+    puts "The line is currently:"+""
   end
 end
 
-
 def take_a_number(a,name)
-  a.each_with_index do |name,index|
-    puts "Welcome #{name}. You are number #{index+1} in line."
-  end
+  a << name
+  puts "Welcome, #{name}. You are number #{a.index(name)}"
 end
